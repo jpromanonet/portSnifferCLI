31da4b2bda5ed8747cd1e865a42a59c9d24a011a
jpromanonet <programming@jpromano.net>
2022-11-07 21:44:26 -0300

    arguments

--- ip_sniffer/src/main.rs
+++ ip_sniffer/src/main.rs
@@ -1,4 +1,11 @@
 use std::env;
+use std::net::IpAddr;
+
+struct Arguments {
+    flag: String,
+    ipaddr: IpAddr,
+    threads: u16,
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
