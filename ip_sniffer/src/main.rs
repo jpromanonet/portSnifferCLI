20a04d60bdf039884e539db0490af6e97e29e1f7
jpromanonet <programming@jpromano.net>
2022-11-07 21:45:20 -0300

    arguments

--- ip_sniffer/src/main.rs
+++ ip_sniffer/src/main.rs
@@ -11,7 +11,4 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
 
-    let flag = args[1].clone();
-    let threads = args[2].clone();
-    let ipaddr = args[3].clone();
 }
