5d6f7417423c1dc8289f57d19deb685d9ba954b6
jpromanonet <programming@jpromano.net>
2022-11-07 21:46:58 -0300

    arguments

--- ip_sniffer/src/main.rs
+++ ip_sniffer/src/main.rs
@@ -7,6 +7,30 @@ struct Arguments {
     threads: u16,
 }
 
+impl Arguments {
+    fn new(args: &[String]) => Result<Arguments, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        } else if args.len() > 4 {
+            return Err("Too many arguments");
+        }
+
+        let flag = args[1].clone();
+        let ipaddr = args[2].clone();
+
+        let threads = if args.len() == 4 {
+            match args[3].parse::<u16>() {
+                Ok(threads) => threads,
+                Err(_) => return Err("Invalid number of threads"),
+            }
+        } else {
+            4
+        };
+
+        Ok(Arguments { flag, ipaddr, threads })
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
