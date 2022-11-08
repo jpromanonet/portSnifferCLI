d2ee18ccddcbc06708ed1d2e6f25d6ad303d61e7
jpromanonet <programming@jpromano.net>
2022-11-07 21:43:25 -0300

    arguments

--- ip_sniffer/src/main.rs
+++ ip_sniffer/src/main.rs
@@ -3,4 +3,8 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
+
+    let flag = args[1].clone();
+    let threads = args[2].clone();
+    let ipaddr = args[3].clone();
 }
